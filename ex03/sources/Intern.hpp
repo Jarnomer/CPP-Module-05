01d92723ec5978e9254007481acb0d7b1cd4df71
Your Name <you@example.com>
2024-11-18 15:15:03 +0200

    Finished ex03

--- ex03/sources/Intern.hpp
+++ ex03/sources/Intern.hpp
@@ -1,10 +1,26 @@
 #pragma once
 
-class Intern {
+#include <iostream>
+#include <string>
 
-public:
+#include <PresidentialPardonForm.hpp>
+#include <RobotomyRequestForm.hpp>
+#include <ShrubberyCreationForm.hpp>
+
+class Intern {
+public: // constructors
   Intern(void);
-  Intern(const Intern &other);
-  Intern &operator=(const Intern &other);
+  Intern(const Intern &other) = delete;
+  Intern &operator=(const Intern &other) = delete;
+
+public: // deconstructor
   ~Intern(void);
+
+public: // makeForm
+  AForm *makeForm(std::string name, std::string target);
+
+private: // nested exception classes
+  class FormNotFoundException : public std::exception {
+    const char *what() const noexcept override;
+  };
 };
--- ex03/sources/main.cpp
+++ ex03/sources/main.cpp
@@ -1,4 +1,39 @@
 #include <AForm.hpp>
 #include <Bureaucrat.hpp>
+#include <Intern.hpp>
+#include <PresidentialPardonForm.hpp>
+#include <RobotomyRequestForm.hpp>
+#include <ShrubberyCreationForm.hpp>
 
-int main(void) { return 0; }
+int main(void) {
+  std::string valid[] = {"shrubbery", "robotomy", "president"};
+  Intern i;
+  AForm *p;
+
+  try {
+    p = i.makeForm("A", "Jackson");
+    delete p;
+  } catch (std::exception &e) {
+    std::cout << "exception caught: " << e.what() << "\n";
+  }
+
+  std::cout << "\n";
+
+  try {
+    p = i.makeForm(valid[2], "Trump");
+    delete p;
+  } catch (std::exception &e) {
+    std::cout << "exception caught: " << e.what() << "\n";
+  }
+
+  std::cout << "\n";
+
+  try {
+    p = i.makeForm(valid[1], "Robocop");
+    delete p;
+  } catch (std::exception &e) {
+    std::cout << "exception caught: " << e.what() << "\n";
+  }
+
+  return 0;
+}
